7dc186f488e4a1a617c4f6ea5a59a928e1f07ea6
pax gole <77360698+paxgole@users.noreply.github.com>
2023-11-28 21:34:42 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,49 +1,10 @@
 import os
-import random
-import cv2
 import numpy as np
+import cv2
 
 from tools import binary_confusion, display_plots
 from filters import get_noise_map_basic, get_sharpened_map, adaptive_median_filter_detection, nonadaptive_median_filter_detection
-
-def sp_noise(image, prob=0.01):
-    '''
-    Add salt and pepper noise to image
-    prob: Probability of the noise
-    source: https://stackoverflow.com/a/27342545
-    '''
-    noise_map = np.zeros(image.shape,np.uint8)
-    output = np.zeros(image.shape,np.uint8)
-    thres = 1 - prob
-    white_noise_count = 0
-    black_noise_count = 0
-    norm_count = 0
-    total_count = 0
-    for i in range(image.shape[0]):
-        for j in range(image.shape[1]):
-            total_count += 1
-            rdn = random.random()
-            if rdn < prob:
-                output[i][j] = 0
-                noise_map[i][j] = 0
-                black_noise_count += 1
-            elif rdn > thres:
-                output[i][j] = 255
-                noise_map[i][j] = 0
-                white_noise_count += 1
-            else:
-                output[i][j] = image[i][j]
-                noise_map[i][j] = 1
-                norm_count += 1
-    
-    print("# of set noise white pixels: ", white_noise_count)
-    print("# of set noise black pixels: ", black_noise_count)
-    print("# of total noise pixels: ", white_noise_count+black_noise_count)
-    print("# of original pixels: ", norm_count)
-    print("# of total pixels: ", total_count)
-
-    return output, noise_map, white_noise_count, black_noise_count, white_noise_count+black_noise_count, norm_count, total_count
-
+from noising import sp_noise, sce_filtering
 
 
 if __name__ == "__main__":
@@ -88,7 +49,11 @@ if __name__ == "__main__":
         print('Confusion Matrix for NAMFD Map')
         print(cell_text_namfd)
 
-        # display_plots(image, noisy_image, noisy, sharped_image, cell_text_sharp, amfd_image, cell_text_amfd, namfd_image, cell_text_namfd)
+        stddev_image = get_noise_map_basic(noisy_image, 5)
+        img_sce_sharp = sce_filtering(noisy_image, 1-sharped_image)
+        img_sce_namfd = sce_filtering(noisy_image, 1-namfd_image)
+
+        display_plots(filename, image, noisy_image, noisy, stddev_image, sharped_image, amfd_image, namfd_image, img_sce_sharp, img_sce_namfd)
 
         datafile = open("results/data.csv", "a")
         data = [filename, photo_h, photo_w, white_noise_count, black_noise_count, total_noise_count, norm_count, total_count, sharped_not_noise_count, sharped_noise_count, 'TN: ' + str(conf_matrix_sharp[0, 0]), 'TP: ' + str(conf_matrix_sharp[1, 1]), 'FN: ' + str(conf_matrix_sharp[1, 0]), 'FP: ' + str(conf_matrix_sharp[0, 1]), amfd_not_noise_count, amfd_noise_count, 'TN: ' + str(conf_matrix_amfd[0, 0]), 'TP: ' + str(conf_matrix_amfd[1, 1]), 'FN: ' + str(conf_matrix_amfd[1, 0]), 'FP: ' + str(conf_matrix_amfd[0, 1]), namfd_not_noise_count, namfd_noise_count, 'TN: ' + str(conf_matrix_namfd[0, 0]), 'TP: ' + str(conf_matrix_namfd[1, 1]), 'FN: ' + str(conf_matrix_namfd[1, 0]), 'FP: ' + str(conf_matrix_namfd[0, 1])]
