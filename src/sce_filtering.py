265866aea159dc8f4ddf9f1e00688f53b2ac93a2
jmrkm <151586755+jmrkm@users.noreply.github.com>
2023-11-28 21:02:36 -0600

    Implement basic filtering algorithm

--- src/sce_filtering.py
+++ src/sce_filtering.py
@@ -6,6 +6,7 @@ module docstring goes here
  
 import cv2
 import numpy as np
+import random
 
 
 def sp_noise(image, prob=0.01):
@@ -15,7 +16,7 @@ def sp_noise(image, prob=0.01):
     source: https://stackoverflow.com/a/27342545
     '''
     output = np.zeros(image.shape,np.uint8)
-    noise_mask = np.zeros(image.shape,np.bool)
+    noise_mask = np.zeros(image.shape,bool)
     thres = 1 - prob
     noise_count = 0
     norm_count = 0
@@ -40,16 +41,14 @@ def sp_noise(image, prob=0.01):
     print("# of set noise pixels: ", noise_count)
     print("# of original pixels: ", norm_count)
     print("# of total pixels: ", total_count)
-        
-    _, binary_matrix = cv2.threshold(noise_map, 128, 255, cv2.THRESH_BINARY)
 
-    noise_map = (binary_matrix > 0).astype(int)
     return output, noise_mask
 
 
 #TODO: add any special parameters we want to be tweaked
 def sce_filtering(
     img: np.uint8,
+    mask: np.bool_,
     # spatial_variance: float,
     # intensity_variance: float,
     # kernel_size: int,
@@ -58,11 +57,34 @@ def sce_filtering(
     function docstring goes here
     """
 
+    img = np.pad(img,2,constant_values=0)
+    mask = np.pad(mask,2,constant_values=True)
+
     img = img / 255
     img = img.astype("float32")
     img_filtered = np.zeros(img.shape) # Placeholder of the filtered image
 
     ### our code goes here
+    for i in range(2, img.shape[0]-2):
+        for j in range(2, img.shape[1]-2):
+            if mask[i,j]:
+                mask_slice = mask[i-2:i+2,j-2:j+2]
+                img_slice = img[i-2:i+2,j-2:j+2]
+
+                samples = []
+                for ii in range(img_slice.shape[0]):
+                    for jj in range(img_slice.shape[1]):
+                        if not mask_slice[ii,jj]:
+                            samples.append(img_slice[ii,jj])
+
+                if len(samples) >= 1:
+                    img_filtered[i,j] = np.median(samples)
+                else:
+                    img_filtered[i,j] = img[i,j]
+            else:
+                    img_filtered[i,j] = img[i,j]
+
+    img_filtered = img_filtered[2:img_filtered.shape[0]-2,2:img_filtered.shape[1]-2]
 
 
     img_filtered = img_filtered * 255
@@ -73,15 +95,19 @@ def sce_filtering(
 if __name__ == "__main__":
     img = cv2.imread("data/boat.png", 0) # read gray image
 
-    # Generate Gaussian noise
-    noise = np.random.normal(0,0.5,img.size)
-    noise = noise.reshape(img.shape[0],img.shape[1]).astype('uint8')
+    # # Generate Gaussian noise
+    # noise = np.random.normal(0,0.5,img.size)
+    # noise = noise.reshape(img.shape[0],img.shape[1]).astype('uint8')
 
-    # Add the generated Gaussian noise to the image
-    img_noise = cv2.add(img, noise)
-    cv2.imwrite('results/im_noisy.png', img_noise)
+    # # Add the generated Gaussian noise to the image
+    # img_noise = cv2.add(img, noise)
+    # cv2.imwrite('results/im_noisy.png', img_noise)
+
+    # add noise to the image, and get the ground truth mask for where the noise is
+    img_noisy, mask = sp_noise(img, 0.2)
 
     # SCE filtering
-    img_sce = sce_filtering(img)
+    img_sce = sce_filtering(img_noisy, mask)
+    cv2.imwrite('results/im_noisy.png', img_noisy)
     cv2.imwrite('results/im_sce.png', img_sce)
 
