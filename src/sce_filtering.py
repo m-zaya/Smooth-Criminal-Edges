1fc08c86900779a74e24ae3460b54643d240d306
jmrkm <151586755+jmrkm@users.noreply.github.com>
2023-11-28 20:12:59 -0600

    Add noise func to sce_filtering

--- src/sce_filtering.py
+++ src/sce_filtering.py
@@ -8,6 +8,45 @@ import cv2
 import numpy as np
 
 
+def sp_noise(image, prob=0.01):
+    '''
+    Add salt and pepper noise to image
+    prob: Probability of the noise
+    source: https://stackoverflow.com/a/27342545
+    '''
+    output = np.zeros(image.shape,np.uint8)
+    noise_mask = np.zeros(image.shape,np.bool)
+    thres = 1 - prob
+    noise_count = 0
+    norm_count = 0
+    total_count = 0
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            total_count += 1
+            rdn = random.random()
+            if rdn < prob:
+                output[i,j] = 0
+                noise_mask[i,j] = True
+                noise_count += 1
+            elif rdn > thres:
+                output[i,j] = 255
+                noise_mask[i,j] = True
+                noise_count += 1
+            else:
+                output[i,j] = image[i,j]
+                noise_mask[i,j] = False
+                norm_count += 1
+    
+    print("# of set noise pixels: ", noise_count)
+    print("# of original pixels: ", norm_count)
+    print("# of total pixels: ", total_count)
+        
+    _, binary_matrix = cv2.threshold(noise_map, 128, 255, cv2.THRESH_BINARY)
+
+    noise_map = (binary_matrix > 0).astype(int)
+    return output, noise_mask
+
+
 #TODO: add any special parameters we want to be tweaked
 def sce_filtering(
     img: np.uint8,
@@ -24,6 +63,7 @@ def sce_filtering(
     img_filtered = np.zeros(img.shape) # Placeholder of the filtered image
 
     ### our code goes here
+
 
     img_filtered = img_filtered * 255
     img_filtered = np.uint8(img_filtered)
